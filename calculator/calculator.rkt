601aed27597dd3bd24aef0aeba8834a0d6adbcfe
Comrade SparklePony <comradesparklepony@gmail.com>
2019-05-26 15:20:09 -0400

    create data types for calculator
    * create Calculator data type
    * create Operation data type

--- calculator/calculator.rkt
+++ calculator/calculator.rkt
@@ -5,6 +5,19 @@
   web-server/servlet
   web-server/servlet-env)
 
+; a Calculator is
+; (calculator result operation input)
+; where result and input are numbers
+; and operation is an Operation
+(struct calculator (result operation input))
+
+; an Operation a String that is one of:
+; - "+" representing addition
+; - "-" representing subtraction
+; - "*" representing multiplication
+; - "/" representing division
+; - "^" representing exponentiation
+
 ;;;;;;;;;;;; model
 
 
