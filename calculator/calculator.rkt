accb16d83d72cedca82ce70ed90b69cfb7190b5d
Comrade SparklePony <comradesparklepony@gmail.com>
2019-05-28 06:41:37 -0400

    add evaluate-operation and evaluate-calculator
    *Add the functions to evaluate operations and evaluate the calculator
    *Add respective tests
    *Make calculator struct transparent so that tests actually work

--- calculator/calculator.rkt
+++ calculator/calculator.rkt
@@ -9,7 +9,7 @@
 ; (calculator result operation input)
 ; where result and input are numbers
 ; and operation is an Operation
-(struct calculator (result operation input))
+(struct calculator (result operation input) #:transparent)
 
 ; an Operation a String that is one of:
 ; - "+" representing addition
@@ -20,7 +20,30 @@
 
 ;;;;;;;;;;;; model
 
+; evaluate-calculator : Calculator -> Calculator
+; this evaluates a calculator by applying it's Operation to its result and input
+(define (evaluate-calculator calc)
+  (calculator
+    (apply-operation
+      (calculator-result calc)
+      (calculator-operation calc)
+      (calculator-input calc))
+    (calculator-operation calc)
+    0))
 
+; apply-operation : Number, Operation, Number -> Number
+; this figures out the result of applying an Operation to two numbers
+; essentially does (operation result input)
+(define (apply-operation num1 op num2)
+  (cond
+    [(string=? op "+") (+ num1 num2)]
+    [(string=? op "-") (- num1 num2)]
+    [(string=? op "*") (* num1 num2)]
+    [(string=? op "/") (if (not (zero? num2))
+			   (/ num1 num2)
+			   0)]
+    [(string=? op "^") (expt num1 num2)]
+    [else num2]))
 
 ;;;;;;;;;;;; view
 
--- calculator/tests.rkt
+++ calculator/tests.rkt
@@ -7,9 +7,41 @@
 
 ;;;;;;;;;;;; model
 
+; evaluate-calculator : Calculator -> Calculator
+; this evaluates a calculator by applying it's Operation to its result and input
+(check-equal?
+  (evaluate-calculator (calculator 5 "+" 5))
+  (calculator 10 "+" 0))
+(check-equal?
+  (evaluate-calculator (calculator 1 "-" 4))
+  (calculator -3 "-" 0))
+(check-equal?
+  (evaluate-calculator (calculator 10 "*" 2))
+  (calculator 20 "*" 0))
+(check-equal?
+  (evaluate-calculator (calculator 1000000 "/" 0))
+  (calculator 0 "/" 0))
+(check-equal?
+  (evaluate-calculator (calculator 4 "^" 0.5))
+  (calculator 2.0 "^" 0))
+
+; apply-operation : Number, Operation, Number -> Number
+; this figures out the result of applying an Operation to two numbers
+; essentially does (operation result input)
+(check-equal?
+  (apply-operation 5 "+" 5) 10)
+(check-equal?
+  (apply-operation 1 "-" 4) -3)
+(check-equal?
+  (apply-operation 10 "*" 2) 20)
+(check-equal?
+  (apply-operation 1000000 "/" 0) 0)
+(check-equal?
+  (apply-operation 4 "^" 0.5) 2.0)
+(check-equal?
+  (apply-operation 5 "nPr" 2) 2)
 
 ;;;;;;;;;;;; view
-(calculator 1 "+" 2)
 
 ; render-calculator-display : Calculator -> Xexpr
 ; this renders the display of the calculator in html
