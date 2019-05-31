951ec94f4c0f116664c94c53941813103264a920
Comrade SparklePony <comradesparklepony@gmail.com>
2019-05-31 15:10:26 -0400

    Add process-request, helper functions, tests
    * add `process-request`
    * add `calculator-request?`
    * add `number-append
    
    Some functions were created in tests.rkt, as wrappers so that the Racket
    web-server testing library could be used.

--- calculator/calculator.rkt
+++ calculator/calculator.rkt
@@ -1,9 +1,12 @@
+; NOTE TO SELF: continue woth process-request
+
 ; language and modules
 #lang racket
 
 (require
   web-server/servlet
-  web-server/servlet-env)
+  web-server/servlet-env
+  web-server/http)
 
 ; a Calculator is
 ; (calculator result operation input)
@@ -18,7 +21,54 @@
 ; - "/" representing division
 ; - "^" representing exponentiation
 
+; a CalculatorRequest is a Request consisting of the following fields:
+; - calculator-result (Number as a Bytes)
+; - calculator-operation (Operation as a Bytes)
+; - calculator-input (Number as a Bytes)
+; - button-pressed (CalculatorButton as a Bytes)
+
+; a CalculatorButton is a String that is one of:
+; - one of Operation, meaning the operation should be changed
+; - "=", representing that a calculator should be evaluated
+; - a 1-digit number, from 0 to 9, which should be appended to the input
+
 ;;;;;;;;;;;; model
+
+; process-request : CalculatorRequest -> Calculator
+; this processes a CalculatorRequest and turns it into a Calculator
+(define (process-request req)
+  (if (calculator-request? req)
+      (let*
+	([binds (request-bindings/raw req)]
+	[cr-result
+	  (string->number
+	    (bytes->string/utf-8
+	      (binding:form-value
+		(bindings-assq #"calculator-result" binds))))]
+	[cr-operation
+	  (bytes->string/utf-8
+	    (binding:form-value
+	      (bindings-assq #"calculator-operation" binds)))]
+	[cr-input
+	  (string->number
+	    (bytes->string/utf-8
+	      (binding:form-value
+		(bindings-assq #"calculator-input" binds))))]
+	[cr-button
+	  (bytes->string/utf-8
+	    (binding:form-value
+	      (bindings-assq #"button-pressed" binds)))])
+	(cond
+	  [(string-contains? "+-/*^" cr-button)            ; button is an operation
+	   (calculator cr-result cr-button cr-input)]
+	  [(string=? "=" cr-button)                        ; button is "="
+	   (evaluate-calculator
+	     (calculator cr-result cr-operation cr-input))]
+	  [(string-contains? "0123456789" cr-button)       ; button is a number
+	   (calculator cr-result
+		       cr-operation
+		       (number-append cr-input (string->number cr-button)))]))
+      (calculator 0 "+" 0)))
 
 ; evaluate-calculator : Calculator -> Calculator
 ; this evaluates a calculator by applying it's Operation to its result and input
@@ -45,6 +95,34 @@
     [(string=? op "^") (expt num1 num2)]
     [else num2]))
 
+; calculator-request? : Any -> Boolean
+; determines if a data type is a CalculatorRequest
+; predicate for CalculatorRequest
+(define (calculator-request? req)
+  (define binds
+    (request-bindings/raw req))
+  (if (request? req)
+      (and
+	(bindings-assq #"calculator-result" binds)
+	(bindings-assq #"calculator-operation" binds)
+	(bindings-assq #"calculator-input" binds)
+	(bindings-assq #"button-pressed" binds)
+	#t)
+      #f))
+
+; boolean->string : Boolean -> String
+; converts a boolean to a string.
+(define (boolean->string b)
+  (if b "#t" "#f"))
+
+; number-append : Number, Number -> Number
+; Takes two numbers as input and appends the second to the first
+(define (number-append num1 num2)
+  (string->number
+    (string-append
+      (number->string num1)
+      (number->string num2))))
+
 ;;;;;;;;;;;; view
 
 ; render-calculator-display : Calculator -> Xexpr
@@ -57,6 +135,10 @@
 	       " "
 	       (calculator-operation calc)))
 	,(number->string (calculator-input calc))))
+
+; render-calculator-keypad : -> Xexpr
+; this renders the keypad in html
+; and will make the proper request when a key is pressed
 
 ;;;;;;;;;;;; run the program
 (define (start req)
--- calculator/tests.rkt
+++ calculator/tests.rkt
@@ -3,9 +3,65 @@
 
 (require
   "calculator.rkt"
-  rackunit)
+  rackunit
+  web-server/test
+  web-server/servlet)
 
 ;;;;;;;;;;;; model
+
+; process-request : CalculatorRequest -> Calculator
+; this processes a CalculatorRequest and turns it into a Calculator
+
+(define (process-request-servlet req) ; servlet wrapper for process-request, for web-server/test
+  (define rcalc (process-request req))
+  (response/xexpr
+    `(li
+       ,(string-append
+	  (number->string (calculator-result rcalc))
+	  (calculator-operation rcalc)
+	  (number->string (calculator-input rcalc))))))
+(define process-request-tester
+  (make-servlet-tester process-request-servlet))
+(check-equal?
+  (process-request-tester
+    "/"
+    (list
+      (binding:form #"calculator-result" #"5")
+      (binding:form #"calculator-operation" #"+")
+      (binding:form #"calculator-input" #"5")
+      (binding:form #"button-pressed" #"7")))
+  '(li
+     ()
+     "5+57"))
+(check-equal?
+  (process-request-tester
+    "/"
+    (list
+      (binding:form #"calculator-result" #"0")
+      (binding:form #"calculator-operation" #"/")
+      (binding:form #"calculator-input" #"50")
+      (binding:form #"button-pressed" #"=")))
+  '(li
+     ()
+     "0/0"))
+(check-equal?
+  (process-request-tester
+    "/"
+    (list))
+  '(li
+     ()
+     "0+0"))
+(check-equal?
+  (process-request-tester
+    "/"
+    (list
+      (binding:form #"calculator-result" #"23")
+      (binding:form #"calculator-operation" #"-")
+      (binding:form #"calculator-input" #"50")
+      (binding:form #"button-pressed" #"*")))
+  '(li
+     ()
+     "23*50"))
 
 ; evaluate-calculator : Calculator -> Calculator
 ; this evaluates a calculator by applying it's Operation to its result and input
@@ -40,6 +96,59 @@
   (apply-operation 4 "^" 0.5) 2.0)
 (check-equal?
   (apply-operation 5 "nPr" 2) 2)
+
+; calculator-request? : Any -> Boolean
+; determines if a data type is a CalculatorRequest
+; predicate for CalculatorRequest
+
+(define (calculator-request?-servlet req)
+  (response/xexpr
+    `(li
+       ,(boolean->string (calculator-request? req)))))
+
+(define calculator-request?-tester
+  (make-servlet-tester calculator-request?-servlet))
+
+(check-equal?
+  (calculator-request?-tester 
+    "/"
+    (list))
+  '(li () "#f"))
+(check-equal?
+  (calculator-request?-tester
+    "/"
+    (list
+      (binding:form #"calculator-result" #"23")
+      (binding:form #"calculator-operation" #"-")
+      (binding:form #"calculator-input" #"50")
+      (binding:form #"button-pressed" #"*")))
+  '(li () "#t"))
+(check-equal?
+  (calculator-request?-tester
+    "/"
+    (list
+      (binding:form #"calculator-operation" #"+")
+      (binding:form #"calculator-input" #"50")))
+  '(li () "#f"))
+(check-equal?
+  (calculator-request?-tester
+    "/"
+    (list
+      (binding:form #"name" #"john smith")
+      (binding:form #"date" #"today")))
+  '(li () "#f"))
+
+; number-append : Number, Number -> Number
+; Takes two numbers as input and appends the second to the first
+(check-equal?
+  (number-append 5 3)
+  53)
+(check-equal?
+  (number-append 0 7)
+  7)
+(check-equal?
+  (number-append 408 3)
+  4083)
 
 ;;;;;;;;;;;; view
 
